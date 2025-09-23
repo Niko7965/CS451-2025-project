cd2977fe198ffb762a22b9c5b8d45437600f2d03
Niko7965 <thehjortshoj@gmail.com>
2025-09-23 07:37:39 +0200

    Tried doing shittylinks

--- template_java/src/main/java/cs451/Main.java
+++ template_java/src/main/java/cs451/Main.java
@@ -24,41 +24,43 @@ private static void initSignalHandlers() {
 }
 
 public static void main(String[] args) throws InterruptedException, SocketException, UnknownHostException {
-        Parser parser = new Parser(args);
-        parser.parse();
+    Parser parser = new Parser(args);
+    parser.parse();
 
-        initSignalHandlers();
+    initSignalHandlers();
 
-        // example
-        long pid = ProcessHandle.current().pid();
-        System.out.println("My PID: " + pid + "\n");
-        System.out.println("From a new terminal type `kill -SIGINT " + pid + "` or `kill -SIGTERM " + pid + "` to stop processing packets\n");
+    // example
+    long pid = ProcessHandle.current().pid();
+    System.out.println("My PID: " + pid + "\n");
+    System.out.println("From a new terminal type `kill -SIGINT " + pid + "` or `kill -SIGTERM " + pid + "` to stop processing packets\n");
 
-        System.out.println("My ID: " + parser.myId() + "\n");
-        System.out.println("List of resolved hosts is:");
-        System.out.println("==========================");
-        for (Host host: parser.hosts()) {
-            System.out.println(host.getId());
-            System.out.println("Human-readable IP: " + host.getIp());
-            System.out.println("Human-readable Port: " + host.getPort());
-            System.out.println();
-        }
+    System.out.println("My ID: " + parser.myId() + "\n");
+    System.out.println("List of resolved hosts is:");
+    System.out.println("==========================");
+    for (Host host: parser.hosts()) {
+        System.out.println(host.getId());
+        System.out.println("Human-readable IP: " + host.getIp());
+        System.out.println("Human-readable Port: " + host.getPort());
         System.out.println();
+    }
+    System.out.println();
 
-        System.out.println("Path to output:");
-        System.out.println("===============");
-        System.out.println(parser.output() + "\n");
+    System.out.println("Path to output:");
+    System.out.println("===============");
+    System.out.println(parser.output() + "\n");
 
-        System.out.println("Path to config:");
-        System.out.println("===============");
-        System.out.println(parser.config() + "\n");
+    System.out.println("Path to config:");
+    System.out.println("===============");
+    System.out.println(parser.config() + "\n");
 
-        System.out.println("Doing some initialization\n");
+    System.out.println("Doing some initialization\n");
 
-        System.out.println("Broadcasting and delivering messages...\n");
+    System.out.println("Broadcasting and delivering messages...\n");
+
+    Thread.sleep(30 * 1000);
 
 
-        System.out.println("Start links");
+    System.out.println("Start links");
         for(Host h: parser.hosts()){
             System.out.println(h.getId());
             if(h.getId() == parser.myId()){
