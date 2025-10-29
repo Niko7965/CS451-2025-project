09bcadb76723f8362299e3b230fb2d04546b62e7
Niko7965 <thehjortshoj@gmail.com>
2025-10-29 14:12:54 +0100

    Flush output every 100 lines

--- template_java/src/main/java/cs451/OutputWriter.java
+++ template_java/src/main/java/cs451/OutputWriter.java
@@ -7,15 +7,22 @@ import java.io.IOException;
 public class OutputWriter {
 //https://www.baeldung.com/java-synchronized was used as reference for sync
 
+    int lineNumber;
     private final BufferedWriter writer;
 
     public OutputWriter(String path) throws IOException {
         writer = new BufferedWriter(new FileWriter(path));
+        lineNumber = 0;
     }
 
     public void write(String s) throws IOException {
         synchronized (writer){
             writer.write(s);
+            lineNumber += 1;
+            if(lineNumber == 100){
+                writer.flush();
+                lineNumber = 0;
+            }
         }
     }
 
