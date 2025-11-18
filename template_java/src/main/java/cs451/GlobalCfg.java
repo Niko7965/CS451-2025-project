392ef2088b3f97025a617a44b22961fffa5a9574
Niko7965 <thehjortshoj@gmail.com>
2025-11-18 09:11:43 +0100

    Add debugging output

--- template_java/src/main/java/cs451/GlobalCfg.java
+++ template_java/src/main/java/cs451/GlobalCfg.java
@@ -2,5 +2,5 @@ package cs451;
 
 public class GlobalCfg {
     public static final boolean PL_ACK_DEBUG = false;
-    public static final boolean URB_ACK_DEBUG = true;
+    public static final boolean URB_ACK_DEBUG = false;
 }
