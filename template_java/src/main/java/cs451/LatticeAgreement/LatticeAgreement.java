33f25b93a8a13641829f4473dd4337ba816d47d9
Niko7965 <thehjortshoj@gmail.com>
2025-12-26 12:28:54 +0100

    add debug print

--- template_java/src/main/java/cs451/LatticeAgreement/LatticeAgreement.java
+++ template_java/src/main/java/cs451/LatticeAgreement/LatticeAgreement.java
@@ -70,7 +70,7 @@ public class LatticeAgreement {
 
     public Optional<Set<Integer>> getDeliverableSet(int noOfProcesses){
         if(GlobalCfg.LA_DBG){
-            System.out.println(ackCount);
+            System.out.println("ack count: "+ackCount);
         }
 
         if(active && ackCount > noOfProcesses / 2){
