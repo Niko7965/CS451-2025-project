2ce7eb1170d1e265aa2a5cbf46fbf1a84a89f6db
Niko7965 <thehjortshoj@gmail.com>
2025-12-26 12:47:47 +0100

    add debug print

--- template_java/src/main/java/cs451/LatticeAgreement/LatticeVoter.java
+++ template_java/src/main/java/cs451/LatticeAgreement/LatticeVoter.java
@@ -1,5 +1,8 @@
 package cs451.LatticeAgreement;
 
+import cs451.GlobalCfg;
+import cs451.Main;
+
 import java.util.Set;
 
 public class LatticeVoter {
@@ -13,10 +16,27 @@ public class LatticeVoter {
 
     private LatticeVote getVoteForProposal(LatticeProposal proposal) {
         Set<Integer> proposalSet = proposal.proposedSet;
+
+        if(GlobalCfg.LA_DBG){
+            System.out.println("Voting:");
+            System.out.println("Proposal:");
+            Main.printSet(proposalSet);
+            System.out.println("Current:");
+            Main.printSet(this.acceptedValue);
+        }
+
         if (proposalSet.containsAll(acceptedValue)) {
+            if(GlobalCfg.LA_DBG){
+                System.out.println("Voted yes");
+            }
+
             acceptedValue = proposalSet;
             return LatticeVote.positiveVoteFromProposal(proposal);
         } else {
+            if(GlobalCfg.LA_DBG){
+                System.out.println("Voted no");
+            }
+
             acceptedValue.addAll(proposalSet);
             return LatticeVote.negativeVoteFromProposal(proposal);
         }
