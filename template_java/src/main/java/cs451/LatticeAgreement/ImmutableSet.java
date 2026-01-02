a6c8c07ae589d182291db7447f245c261fe01839
Niko7965 <thehjortshoj@gmail.com>
2026-01-02 21:27:53 +0100

    dbg

--- template_java/src/main/java/cs451/LatticeAgreement/ImmutableSet.java
+++ template_java/src/main/java/cs451/LatticeAgreement/ImmutableSet.java
@@ -18,13 +18,7 @@ public class ImmutableSet implements Serializable {
     public ImmutableSet(Collection<Integer> collection){
         this.inner = new HashSet<>();
         inner.addAll(collection);
-        System.out.println("inner:");
-        Main.printSet(inner);
 
-        System.out.println("given:");
-        for(Integer i :collection){
-            System.out.print(i+"");
-        }
     }
 
     public Set<Integer> getInner(){
